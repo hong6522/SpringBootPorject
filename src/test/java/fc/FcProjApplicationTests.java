54a17d25246c73a234cb0659290d96984e369459
kinbali <hgh4400@naver.com>
2023-07-03 12:19:11 +0900

    start

---
+++ src/test/java/fc/FcProjApplicationTests.java
@@ -0,0 +1,13 @@
+package fc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
