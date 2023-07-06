64100069ac20276b0f5f67cc8b549a2b31d48fab
kinbali <hgh4400@naver.com>
2023-07-06 14:55:49 +0900

    메인페이지 카테고리별 리스트 출력

--- src/main/java/fc/control/FCController.java
+++ src/main/java/fc/control/FCController.java
@@ -1,18 +1,31 @@
 package fc.control;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fc.db.ProductDTO;
+import fc.db.ProductMapper;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class FCController {
-
+	
+	@Resource
+	ProductMapper pm;
+	
 	@RequestMapping("/mainPage")
 	String index(HttpSession session) {
 		System.out.println("session:"+session.getAttribute("type"));
 		return "mainPage";
 	}
 	
-	
+	@RequestMapping("/mainPage/{kind}")
+	String index(Model mm, ProductDTO dto) {
+		
+		mm.addAttribute("mainData", pm.pro_select(dto));
+		
+		return "mainPage";
+	}
 }
