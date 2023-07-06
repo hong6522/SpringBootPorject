4def8d13f8b83b8f06eb9a2bce98cd1b295877a5
chanb1108 <chanb1108@naver.com>
2023-07-06 14:29:18 +0900

    수정(git 파일 연결중)

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -21,3 +21,35 @@ CREATE TABLE qna (
     answer_Date date, -- 답변 등록일
     admin_Id varchar(100) -- 답변 작성한 관리자id
 );
+
+CREATE TABLE fassion_review (
+    no int auto_increment primary key, --  번호
+    order_Code varchar(100),
+    fashion_Name varchar(100),   -- 상품명
+    title varchar(100) , -- 제목
+    content varchar(999), -- 내용
+    id varchar(100) , -- 아이디
+    star_num int(11) , -- 평가 별점
+    review_cnt int(11) , -- 조회수
+    reg_date date, -- 등록일자
+    upfile varchar(100), -- 파일1
+    upfile1 varchar(100), -- 파일2
+    upfile2 varchar(100) -- 파일3
+); 
+
+CREATE table product(
+    kind varchar(100),
+    proKind varchar(100),
+    proName varchar(100),
+    proDetail varchar(100),
+    proImg varchar(100),
+    proSize varchar(100),
+    proColor varchar(100),
+    color varchar(100),
+    sel varchar(100),
+    memPoint varchar(100),
+    num int auto_increment primary key,
+    supplyPrice int(11),
+    sellPrice int(11),
+    proCnt int(11)
+);
